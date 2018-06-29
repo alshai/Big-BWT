1cd8b78f19f3a951f5d23e3bf72aba30be818cae
giovanni <giovanni.manzini@gmail.com>
2018-06-29 19:00:54 +0200

    forgotten utils.[ch] added

---
+++ utils.c
@@ -0,0 +1,27 @@
+#define _GNU_SOURCE
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <assert.h>
+
+
+// write error message and exit
+void die(const char *s)
+{
+  perror(s);
+  exit(1);
+}    
+
+
+// open and return an auxiliary file
+// open file named base.ext with mode mode
+FILE *open_aux_file(const char *base, const char *ext, const char *mode)
+{
+  char *name;
+  int e = asprintf(&name,"%s.%s",base,ext);
+  if(e<1) die("asprint error");
+  FILE *f = fopen(name,mode);
+  if(f==NULL) die(name);  
+  free(name);
+  return f;
+}
